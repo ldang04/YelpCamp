9aa48190d56d613a57e0eac15e16426feac15710
Linh <dangdiemlinh04@gmail.com>
2020-09-03 16:03:59 +0000

    update db port

--- app.js
+++ app.js
@@ -19,8 +19,9 @@ var commentRoutes    = require("./routes/comments"),
 
 // console.log(process.env.DATABASEURL);
 
-mongoose.connect('mongodb://localhost:27017/yelp_camp'
 // mongoose.connect('mongodb+srv://ldang04:MongodbPa$$1778@cluster0.ilqnw.mongodb.net/<dbname>?retryWrites=true&w=majority'
+
+mongoose.connect('process.env.DATABASEURL'
 , {
   useNewUrlParser: true,
   useUnifiedTopology: true
