b15343a4993548fa602abb949e72ae9935cba26b
Linh <dangdiemlinh04@gmail.com>
2020-09-03 10:57:57 +0000

    add mongolab

--- app.js
+++ app.js
@@ -16,9 +16,10 @@ const mongoose = require('mongoose');
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
- 
 
-mongoose.connect('mongodb://localhost:27017/yelp_camp', {
+// mongoose.connect('mongodb://localhost:27017/yelp_camp'
+mongoose.connect( "mongodb+srv://ldang04:MongodbPa$$1778@cluster0.ilqnw.mongodb.net/<dbname>?retryWrites=true&w=majority"
+, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
