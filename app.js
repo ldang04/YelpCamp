f6c5248948744b457f9a45f9a92d64adb15057dd
Linh <dangdiemlinh04@gmail.com>
2020-09-03 15:45:10 +0000

    update dburl

--- app.js
+++ app.js
@@ -17,8 +17,10 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
 
-// mongoose.connect('mongodb://localhost:27017/yelp_camp'
-mongoose.connect('mongodb+srv://ldang04:MongodbPa$$1778@cluster0.ilqnw.mongodb.net/<dbname>?retryWrites=true&w=majority'
+// console.log(process.env.DATABASEURL);
+
+mongoose.connect('mongodb://localhost:27017/yelp_camp'
+// mongoose.connect('mongodb+srv://ldang04:MongodbPa$$1778@cluster0.ilqnw.mongodb.net/<dbname>?retryWrites=true&w=majority'
 , {
   useNewUrlParser: true,
   useUnifiedTopology: true
