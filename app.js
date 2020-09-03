fe61cc64987c67e70ab4646e7fa13e7fe0ed4405
Linh <dangdiemlinh04@gmail.com>
2020-09-03 16:20:04 +0000

    change port

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ var commentRoutes    = require("./routes/comments"),
 
 // mongoose.connect('mongodb+srv://ldang04:MongodbPa$$1778@cluster0.ilqnw.mongodb.net/<dbname>?retryWrites=true&w=majority'
 
+// mongoose.connect("mongodb://localhost:27017/yelp_camp");
 mongoose.connect('process.env.DATABASEURL'
 , {
   useNewUrlParser: true,
